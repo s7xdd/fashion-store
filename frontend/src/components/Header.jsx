e1fc4f8f326883384d94c56965f5b0ce0dfac24c
Mohammed Shamil <kmshamil123@gmail.com>
2024-09-30 22:09:28 +0530

    update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import logo from '../assets/logo.svg'
+import logo from '../assets/logo.webp'
 import Navbar from './Navbar'
 import { MdClose, MdMenu } from 'react-icons/md'
 import { FaCartShopping } from 'react-icons/fa6'
@@ -13,9 +13,8 @@ const Header = () => {
   return (
     <header className='fixed top-0 left-0 m-auto  w-full bg-white ring-1 ring-slate-900/5 z-10 '>
         <div className='px-4 flex items-center justify-between py-3 max-xs:px-2'>
-            {/*Logo*/}
             <div>
-                <Link>
+                <Link to={'/'}>
                     <img src={logo} alt='' height={66} width={60}/>
                 </Link>
             </div>
